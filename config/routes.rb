348828f40f9f2b4cb436c6f731f3f4012092d764
Erwin Gatica <erwin.gmanz@gmail.com>
2021-07-21 17:52:57 -0400

    controller actions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # read
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks', to: 'tasks#show', as: :task
+
+  # create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # update
+  get 'tasks/edit'
+  patch 'tasks', to: 'tasks#update'
+
+  # delete
+  delete 'tasks', to: 'tasks#destroy'
 end
